e46cb6d64511d447f8280b7276f95838b0f523df
Ryota Egusa <egusa.ryota@gmail.com>
2020-04-21 00:25:19 +0900

    Remove unused code

--- src/main/java/jp/sawa_kai/sawakai/spigot/skyway/SkyWayClient.kt
+++ src/main/java/jp/sawa_kai/sawakai/spigot/skyway/SkyWayClient.kt
@@ -13,69 +13,14 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.logging.HttpLoggingInterceptor
 import org.bukkit.Bukkit
+import org.json.JSONObject
 import java.io.IOException
-import java.net.CookieHandler
-import java.net.CookieManager
-import java.net.CookiePolicy
 import java.security.SecureRandom
 
 
 class SkyWayClient(private val apiKey: String, private val roomId: String) {
 
-    fun connect() {
-        Bukkit.getLogger().info("Signaling server is $signalingServerUrl")
-
-        val token = SecureRandom.getInstanceStrong()
-            .nextLong()
-            .toString(36)
-            .substring(2)
-
-        IO.setDefaultOkHttpCallFactory(okHttpClient)
-        IO.setDefaultOkHttpWebSocketFactory(okHttpClient)
-        val socket = IO.socket(signalingServerUrl, IO.Options().apply {
-            query = "apiKey=${apiKey}&token=${token}&platform=javascript&sdk_version=2.0.5"
-            timestampRequests = true
-            secure = true
-            forceNew = true
-        })
-        socket.on(Socket.EVENT_CONNECT, Emitter.Listener {
-            socket.emit("ROOM_JOIN", "{\"roomName\":\"${roomId}\",\"roomType\":\"sfu\"}")
-            Bukkit.getLogger().info("connected!")
-        }).on(Socket.EVENT_ERROR, Emitter.Listener {
-            Bukkit.getLogger().info("error")
-            Bukkit.getLogger().info(it.map { it.toString() }.toString())
-        }).on(Socket.EVENT_DISCONNECT) {
-            Bukkit.getLogger().info("Socket was disconnected")
-        }.on(Socket.EVENT_MESSAGE) {
-            Bukkit.getLogger().info("message: ${it}")
-        }.on(Socket.EVENT_PING) {
-            Bukkit.getLogger().info("ping connected?: ${socket.connected()}")
-            Bukkit.getLogger().info("{\"roomName\":\"${roomId}\",\"roomType\":\"sfu\"}")
-            socket.emit("ROOM_JOIN", "{\"roomName\":\"${roomId}\",\"roomType\":\"sfu\"}")
-        }.on(Socket.EVENT_PONG) {
-            Bukkit.getLogger().info("pong")
-        }.on(Socket.EVENT_RECONNECTING) {
-            Bukkit.getLogger().info("reconnecting")
-        }.on(Socket.EVENT_CONNECTING) {
-            Bukkit.getLogger().info("connecting")
-        }.on("ROOM_USER_JOIN") {
-            Bukkit.getLogger().info("ROOM_USER_JOIN")
-            socket.emit("SFU_GET_OFFER", "{\"roomName\":\"${roomId}\"}")
-        }
-        socket.connect()
-
-        GlobalScope.launch {
-            while (true) {
-                socket.emit("PING")
-                delay(25_000)
-            }
-        }
-    }
-
     private val okHttpClient = {
-        val cookieManager = CookieManager()
-        cookieManager.setCookiePolicy(CookiePolicy.ACCEPT_ALL)
-        CookieHandler.setDefault(cookieManager)
         val loggingInterceptor = HttpLoggingInterceptor(object : HttpLoggingInterceptor.Logger {
             override fun log(message: String) {
                 Bukkit.getLogger().info("okhttp: $message")
@@ -88,38 +33,7 @@ class SkyWayClient(private val apiKey: String, private val roomId: String) {
             .cookieJar(CookieJar())
             .addInterceptor {
                 val newRequest = it.request().newBuilder()
-                    .addHeader(
-                        "Accept-Encoding",
-                        "gzip, deflate, br"
-                    )
-                    .addHeader(
-                        "Accept-Language",
-                        "ja-JP,ja;q=0.9,en-US;q=0.8,en;q=0.7,zh-TW;q=0.6,zh;q=0.5,pt;q=0.4"
-                    )
-                    .addHeader(
-                        "Cache-Control",
-                        "no-cache"
-                    )
-                    .addHeader("DNT", "1")
                     .addHeader("Origin", "https://minecraft-voice-chat-test.web.app")
-                    .addHeader("Pragma", "no-cache")
-                    .addHeader("Referer", "https://minecraft-voice-chat-test.web.app/room/")
-                    .addHeader(
-                        "Sec-Fetch-Dest",
-                        "empty"
-                    )
-                    .addHeader(
-                        "Sec-Fetch-Mode",
-                        "cors"
-                    )
-                    .addHeader(
-                        "Sec-Fetch-Site",
-                        "cross-site"
-                    )
-                    .addHeader(
-                        "User-Agent",
-                        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.113 Safari/537.36"
-                    )
                     .build()
 
                 val resp = it.proceed(newRequest)
@@ -149,5 +63,71 @@ class SkyWayClient(private val apiKey: String, private val roomId: String) {
 
     private val signalingServerUrl by lazy {
         "https://${signalingServerDomain}"
+    }
+
+    private val onConnectedHandler: (() -> Unit)? = null
+    private val onUserJoinedRoomHandler: (() -> Unit)? = null
+    private val onUserLeftRoomHandler: (() -> Unit)? = null
+
+    fun connect() {
+        Bukkit.getLogger().info("Signaling server is $signalingServerUrl")
+
+        val token = SecureRandom.getInstanceStrong()
+            .nextLong()
+            .toString(36)
+            .substring(2)
+
+        IO.setDefaultOkHttpCallFactory(okHttpClient)
+        IO.setDefaultOkHttpWebSocketFactory(okHttpClient)
+        val socket = IO.socket(signalingServerUrl, IO.Options().apply {
+            query = "apiKey=${apiKey}&token=${token}"
+            timestampRequests = true
+            secure = true
+            forceNew = true
+        })
+        socket.on(Socket.EVENT_CONNECT, Emitter.Listener {
+            Bukkit.getLogger().info("connected!")
+            val obj = JSONObject().apply {
+                put("roomName", roomId)
+                put("roomType", "sfu")
+            }
+            Bukkit.getLogger().info("RoomId ${roomId}")
+            socket.emit("ROOM_JOIN", obj)
+        }).on(Socket.EVENT_ERROR, Emitter.Listener {
+            Bukkit.getLogger().info("error")
+            Bukkit.getLogger().info(it.map { it.toString() }.toString())
+        }).on(Socket.EVENT_DISCONNECT) {
+            Bukkit.getLogger().info("Socket was disconnected")
+        }.on(Socket.EVENT_MESSAGE) {
+            Bukkit.getLogger().info("message: ${it}")
+        }.on(Socket.EVENT_PING) {
+            Bukkit.getLogger().info("ping")
+        }.on(Socket.EVENT_PONG) {
+            Bukkit.getLogger().info("pong")
+        }.on(Socket.EVENT_RECONNECTING) {
+            Bukkit.getLogger().info("reconnecting")
+        }.on(Socket.EVENT_CONNECTING) {
+            Bukkit.getLogger().info("connecting")
+        }.on("ROOM_USER_JOIN") {
+            Bukkit.getLogger().info("ROOM_USER_JOIN, ${it[0]}")
+            //socket.emit("SFU_GET_OFFER", "{\"roomName\":\"${roomId}\"}")
+            val obj = JSONObject().apply {
+                put("roomName", roomId)
+                put("data", "from minecraft")
+            }
+            socket.emit("ROOM_SEND_DATA", obj)
+        }
+        socket.connect()
+
+        GlobalScope.launch {
+            while (true) {
+                socket.emit("PING")
+                delay(25_000)
+            }
+        }
+    }
+
+    fun onConnected(handler: () -> Unit): SkyWayClient {
+        return this
     }
 }
